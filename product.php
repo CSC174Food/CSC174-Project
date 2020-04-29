f07064d9ce10ef13697981c9134a5a36a99ad18a
heavensevidence <alankarlo03@gmail.com>
2020-04-28 20:30:09 -0700

    Update product.php

--- product.php
+++ product.php
@@ -33,13 +33,14 @@
 
         <?php foreach($product as $pid): ?>
 
-            <div class="col s6 md3">
+            <div class="col s4 md3">
                 <div class="card z-depth-0">
                  <div class="card-content center">
-                 <div class="product-image"><img src="<?php echo $pid['photo']; ?>"></div>
-                 
-                    <h6 ><?php echo $pid['photo']; ?></h6>
-                    <div><?php echo htmlspecialchars($pid['product_name']); ?></div>
+                 <div class="product-image"><img src="<?php echo $pid['photo']; ?>"></div> 
+                    <div><h5><?php echo ($pid['product_name']); ?></h5></div>
+                    <div class="cart-action" style ="width:10%">
+                    <input type="text" class="product-quantity" name="quantity" value="1" />
+                    <input type="submit"  value="add" name="add" class="btn" /></div>
                   </div>
                   <div class="card-action right-align"></div>
                   <a href="#" class="brand-text">more info</a>
