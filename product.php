dce10636e9e1e1e7210e1bea8e0805b3ccc3b44a
heavensevidence <alankarlo03@gmail.com>
2020-04-29 21:15:04 -0700

    Update product.php

--- product.php
+++ product.php
@@ -59,11 +59,12 @@
             <div class="col s4 md3">
                 <div class="card z-depth-0">
                  <div class="card-content center">
+                 
                  <div class="product-image"><img src="<?php echo $pid['photo']; ?>"></div> 
                     <div><h5><?php echo ($pid['product_name']); ?></h5></div>
                     <div class="cart-action" style ="width:10%">
                     <input type="text" class="product-quantity" name="quantity" value="1" />
-                    <input type="submit"  value="add" name="add-cart" class="btn" /></div>
+                    <input type="submit" value="add" name="add-cart" class="btn" /></div>
                   </div>
                   <div class="card-action right-align"></div>
                   <a href="#" class="brand-text">more info</a>
